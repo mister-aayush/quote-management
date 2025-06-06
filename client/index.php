52926155c436e6c18fc86a9f89eb8bed6ddbadf6
Aayush Shrestha <learn.aayush@gmail.com>
2025-06-06 22:03:39 +0545

    background iumage edited

--- client/index.php
+++ client/index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <!-- Fullscreen Background -->
-    <div class="relative flex h-screen w-full items-center justify-center bg-cover bg-center" style="background-image: url('https://images.unsplash.com/photo-1743878206228-5f36b5f5c830?q=80&w=3646&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
+    <div class="relative flex h-screen w-full items-center justify-center bg-cover bg-center" style="background-image: url(image/background.jpeg) ">
         <div class="absolute top-0 right-0 bottom-0 left-0 bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0.6)_50%,_transparent_90%)]"></div>
         <!-- Radial Gradient Content Wrapper -->
         <div class="relative z-30 flex w-full max-w-2xl flex-col items-center rounded-2xl px-8 py-5 text-center">
